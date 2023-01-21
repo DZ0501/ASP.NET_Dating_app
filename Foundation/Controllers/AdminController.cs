fe55bd030545cd80fff5b64ec42ab610222040a5
DZ0501 <dawid.zmuda@microsoft.wsei.edu.pl>
2023-01-21 16:10:55 +0100

    Identity configuration #2

---
+++ Foundation/Controllers/AdminController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using Foundation.Areas.Identity.Data;
+
+namespace Foundation.Controllers
+{
+    public class AdminController : Controller
+    {
+        private RoleManager<IdentityRole> roleManager;
+        public AdminController(RoleManager<IdentityRole> roleMgr)
+        {
+            roleManager = roleMgr;
+        }
+
+        public ViewResult Index() => View(roleManager.Roles);
+
+        private void Errors(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+                ModelState.AddModelError("", error.Description);
+        }
+
+        public IActionResult Create() => View();
+
+        [HttpPost]
+        public async Task<IActionResult> Create([Required] string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result = await roleManager.CreateAsync(new IdentityRole(name));
+                if (result.Succeeded)
+                    return RedirectToAction("Index");
+                else
+                    Errors(result);
+            }
+            return View(name);
+        }
+    }
+}
